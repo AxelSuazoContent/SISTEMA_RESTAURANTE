6a102e6ce5588d238ff4239ef1d33a5b1f9eabb9
axel suazo <114781582+AxelSuazoContent@users.noreply.github.com>
2026-04-08 15:51:22 -0600

    Update index.blade.php

--- restaurante-laravel/resources/views/admin/Facturas/index.blade.php
+++ restaurante-laravel/resources/views/admin/Facturas/index.blade.php
@@ -6,7 +6,7 @@
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
     <h1 class="h2"><i class="bi bi-receipt"></i> Facturas</h1>
 </div>
-
+ 
 <div class="card">
     <div class="card-body p-0">
         <div class="table-responsive">
